feec5d9b5f107d39ec82d275349e4531e86d241e
Jeremie Corkery <jeremiecorkery14@gmail.com>
2021-10-12 04:22:18 -0400

    Continued work, made progredd on fixing dataloading

--- Assignment 2/src/EvenNet.py
+++ Assignment 2/src/EvenNet.py
@@ -1,33 +1,43 @@
-# Write your assignment here
-import argparse
+import numpy as np
+import matplotlib.pyplot as mpl
 import torch
 import torch.nn as nn
 import torch.nn.functional as func
 
 class EvenNet(nn.Module):
-    '''
-    Neural network class.
-    Architecture:
-        Two fully-connected layers fc1 and fc2.
-        Two nonlinear activation functions relu and sigmoid.
-    '''
+   
 
     def __init__(self):
-        super(Net, self).__init__()
-        self.fc1= nn.Linear(196, 25)
-        self.fc2= nn.Linear(25, 5)
+        super(EvenNet, self).__init__()
+       
+        self.inputlayer = nn.Linear(168,75)
+        self.hl1 = nn.Linear(75,40)
+        self.hl2 = nn.Linear(40,20)
+        self.outlayer = nn.Linear(20,5)
 
-    # Feedforward function
+       
+    
     def forward(self, x):
-        h = func.relu(self.fc1(x))
-        y = torch.sigmoid(self.fc2(h))
+        x = self.inputlayer(x)
+        x = self.relu(x)
+
+        h = self.hl1(x)
+        h = self.relu(x)
+
+        g = self.hl2(h)
+        g = self.relu(g)
+        
+        y = torchself.outlayer(g)
+
         return y
 
     # Reset function for the training weights
     # Use if the same network is trained multiple times.
     def reset(self):
-        self.fc1.reset_parameters()
-        self.fc2.reset_parameters()
+        self.inputlayer.reset_parameters()
+        self.hl1.reset_parameters()
+        self.hl2.reset_parameters()
+        self.outlayer.reset_parameters()
 
     # Backpropagation function
     def backprop(self, data, loss, epoch, optimizer):
@@ -36,7 +46,8 @@ class EvenNet(nn.Module):
         targets= torch.from_numpy(data.y_train)
 
         obj_val= loss(self.forward(inputs).reshape(-1), targets)
-        optimizer.zero_grad()
+        for param in self.model.parameters():
+            param.grad = None
         obj_val.backward()
         optimizer.step()
         return obj_val.item()
@@ -51,14 +62,4 @@ class EvenNet(nn.Module):
             cross_val= loss(self.forward(inputs).reshape(-1), targets)
         return cross_val.item()
 
-'''
-
-if __name__ == '__main__':
-    # Command line arguments
-    parser = argparse.ArgumentParser(description='Assignment 1 input parser')
-
-    parser.add_argument('paramfilepath',  help='A file path to the parameter file')
-    
-   
-    args = parser.parse_args()
-'''
+test = EvenNet()
