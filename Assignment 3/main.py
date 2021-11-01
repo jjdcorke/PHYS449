7731abe461f48bacc97801028811ac9f5d791b4c
Jeremie Corkery <jeremiecorkery14@gmail.com>
2021-11-01 08:45:13 -0400

    Started

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -1,4 +1,22 @@
 # Write your assignment here
+import numpy as np
+import matplotlib.pyplot as mpl
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import argparse, json
 
+class VectorField:
+    def __init__(self,u,v):
+        self.u = u
+        self.v = v
+
+class OdeNet(nn.Module):
+
+    def __init__(self):
+        super(OdeNet, self).__init__() 
+
+        self.inputlayer = nn.Linear(2,
+        
 if __name__ == '__main__':
     print('hello')
